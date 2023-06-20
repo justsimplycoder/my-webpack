6b5e9c9ae015c886508b021db135c52550b6c39f
justsimplycoder <justsimplycoder@gmail.com>
2023-06-20 12:06:38 +0400

    Изменение: 2 конфига для development и production разработки

--- webpack.development.js
+++ webpack.development.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
 	// режим разработки 'development', 'production', 'none'
-	mode: 'production',
+	mode: 'development',
 	// точка входа
 	entry: {
 		// Точка входа для javascript
@@ -27,32 +27,6 @@ module.exports = {
 			// Изображения
 			{
 				test: /\.(jpe?g|png|webp|gif|svg)$/i,
-				use: [
-					{
-						// Оптимизация изображений
-						loader: 'image-webpack-loader',
-						options: {
-							mozjpeg: {
-								progressive: true,
-							},
-							// optipng.enabled: false will disable optipng
-							optipng: {
-								enabled: false,
-							},
-							pngquant: {
-								quality: [0.65, 0.90],
-								speed: 4
-							},
-							gifsicle: {
-								interlaced: false,
-							},
-							// the webp option will enable WEBP
-							webp: {
-								quality: 75
-							}
-						}
-					}
-				],
 				type: 'asset/resource',
 				generator: {
 					filename: 'img/[name][ext]'
@@ -60,8 +34,8 @@ module.exports = {
 			},
 			// CSS, PostCSS, Sass
 			{
-				test: /\.(scss|css)$/,
-				use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+				test: /\.(sa|sc|c)ss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
 			},
 		],
 	},
@@ -71,7 +45,7 @@ module.exports = {
 		new CleanWebpackPlugin(),
 		// Плагин создания HTML на основе шаблона
 		new HtmlWebpackPlugin({
-			title: 'Заголовок страницы',
+			title: 'My webpack!',
 			template: path.resolve(__dirname, './src/template.html'), // шаблон
 			filename: 'index.html', // название выходного файла
 		}),
---
+++ webpack.production.js
@@ -0,0 +1,84 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+	// режим разработки 'development', 'production', 'none'
+	mode: 'production',
+	// точка входа
+	entry: {
+		// Точка входа для javascript
+		main: path.resolve(__dirname, './src/index.js'),
+	},
+	// точка выхода
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: '[name].bundle.js',
+	},
+	// модули и загрузчики
+	module: {
+		rules: [
+			// JavaScript
+			{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					use: ['babel-loader'],
+			},
+			// Изображения
+			{
+				test: /\.(jpe?g|png|webp|gif|svg)$/i,
+				use: [
+					{
+						// Оптимизация изображений
+						loader: 'image-webpack-loader',
+						options: {
+							mozjpeg: {
+								progressive: true,
+							},
+							// optipng.enabled: false will disable optipng
+							optipng: {
+								enabled: false,
+							},
+							pngquant: {
+								quality: [0.65, 0.90],
+								speed: 4
+							},
+							gifsicle: {
+								interlaced: false,
+							},
+							// the webp option will enable WEBP
+							webp: {
+								quality: 75
+							}
+						}
+					}
+				],
+				type: 'asset/resource',
+				generator: {
+					filename: 'img/[name][ext]'
+				}
+			},
+			// CSS, PostCSS, Sass
+			{
+				test: /\.(sa|sc|c)ss$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+			},
+		],
+	},
+	// плагины
+	plugins: [
+		// Плагин очистки директории dist при каждой сборке
+		new CleanWebpackPlugin(),
+		// Создание минифицированного css файла
+		new MiniCssExtractPlugin({
+			filename: "css/main.bundle.css",
+		}),
+		// Плагин создания HTML на основе шаблона
+		new HtmlWebpackPlugin({
+			title: 'My webpack!',
+			template: path.resolve(__dirname, './src/template.html'), // шаблон
+			filename: 'index.html', // название выходного файла
+		}),
+	],
+}
